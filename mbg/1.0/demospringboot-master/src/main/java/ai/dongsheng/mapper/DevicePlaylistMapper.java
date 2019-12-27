df8ef43dd475432c33e6a7763caa1f8bff69ef8b
zhoubinbin <18598272696@163.com>
2019-12-27 14:01:59 +0800

    完善

---
+++ mbg/1.0/demospringboot-master/src/main/java/ai/dongsheng/mapper/DevicePlaylistMapper.java
@@ -0,0 +1,7 @@
+package ai.dongsheng.mapper;
+
+import ai.dongsheng.model.entity.DevicePlaylist;
+import com.example.sl.demospringboot.util.MyMapper;
+
+public interface DevicePlaylistMapper extends MyMapper<DevicePlaylist> {
+}
---
+++ mbg/1.0/demospringboot-master/src/main/java/ai/dongsheng/mapper/PayOrdersMapper.java
@@ -0,0 +1,7 @@
+package ai.dongsheng.mapper;
+
+import ai.dongsheng.model.entity.PayOrders;
+import com.example.sl.demospringboot.util.MyMapper;
+
+public interface PayOrdersMapper extends MyMapper<PayOrders> {
+}
