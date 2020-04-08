5a038dc811fa2af2a11d9941b3815da31d38f311
binbin <michellejou@126.com>
2020-04-08 17:36:26 +0800

    新增一个springboot整合HttpClient

---
+++ springboot-http/src/main/java/com/refined/springboot_http/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.refined.springboot_http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ springboot-http/src/test/java/com/refined/springboot_http/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refined.springboot_http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
